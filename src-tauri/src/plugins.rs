643379245c40208563c98501bcdf893b994bae49
Nguyen Thanh Tung <tung.nguyent03@gmail.com>
2024-12-08 18:13:45 +0700

    show app on second launch

--- src-tauri/src/plugins.rs
+++ src-tauri/src/plugins.rs
@@ -1,7 +1,6 @@
-use tauri::{Builder, Wry};
+use tauri::{Builder, Manager, Wry};
 
 use tauri_plugin_autostart::MacosLauncher;
-
 
 pub trait RegisterPlugins {
     fn register_plugins(self) -> Self;
@@ -9,13 +8,15 @@ pub trait RegisterPlugins {
 
 impl RegisterPlugins for Builder<Wry> {
     fn register_plugins(self) -> Self {
-        self
-            .plugin(tauri_plugin_autostart::init(
-                MacosLauncher::LaunchAgent,
-                None,
-            ))
-            .plugin(tauri_plugin_single_instance::init(|_, _, _| {}))
-            .plugin(tauri_plugin_shell::init())
+        self.plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            None,
+        ))
+        .plugin(tauri_plugin_single_instance::init(|app_handle, _, _| {
+            if let Some(window) = app_handle.get_webview_window("main") {
+                let _ = window.show();
+            }
+        }))
+        .plugin(tauri_plugin_shell::init())
     }
 }
-
